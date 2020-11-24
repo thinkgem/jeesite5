51e1f4cbaa05075d858bed084dd3f9d79c791136
thinkgem <thinkgem@163.com>
2020-11-24 15:55:22 +0800

    #I269VL

--- common/src/main/java/com/jeesite/common/lang/DateUtils.java
+++ common/src/main/java/com/jeesite/common/lang/DateUtils.java
@@ -244,7 +244,7 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 		}
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
-		calendar.set(Calendar.HOUR, 0);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
 		calendar.set(Calendar.MINUTE, 0);
 		calendar.set(Calendar.SECOND, 0);
 		calendar.set(Calendar.MILLISECOND, 0);
