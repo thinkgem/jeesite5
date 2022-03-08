55dd0773d324d62f07e53deb07d0012ebf7249fe
thinkgem <thinkgem@163.com>
2022-03-08 17:01:43 +0800

    取消webuploader上传超时参数 #I4WP8B

--- modules/core/src/main/resources/static/ueditor/1.4/third-party/webuploader/webuploader.js
+++ modules/core/src/main/resources/static/ueditor/1.4/third-party/webuploader/webuploader.js
@@ -2971,9 +2971,9 @@
             method: 'POST',
     
             // 跨域时，是否允许携带cookie, 只有html5 runtime才有效
-            withCredentials: false,
+            withCredentials: true,
             fileVal: 'file',
-            timeout: 2 * 60 * 1000,    // 2分钟
+            timeout: 0, //2 * 60 * 1000,    // 2分钟
             formData: {},
             headers: {},
             sendAsBinary: false
